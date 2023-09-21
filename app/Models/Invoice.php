7844bb7f6524ec28f01d0748bba9a9385b99d3c5
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-21 18:22:48 +0430

    Add customer  model

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  public function customer() {
+    return $this->belongsTo(Customer::class);
+  }
 }
