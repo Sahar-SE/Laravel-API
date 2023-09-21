d2cf5dc98a87b5a08bfaf144951fbff03cb3a80a
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-21 23:26:10 +0430

    Add seed

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -13,6 +13,9 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Customer::factory()
+            ->count(50)
+            ->hasInvoices(5)
+            ->create();
     }
 }
