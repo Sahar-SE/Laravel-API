f060c8e6db1c576cb719d791ed693eb050c4eaa6
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-21 18:52:34 +0430

    Add customer factory

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -13,8 +13,16 @@ class CustomerFactory extends Factory
      */
     public function definition()
     {
+        $type = $this->faker->randomElement(['individual', 'business']);
+        $name = $type == 'individual' ? $this->faker->name() : $this->faker->businessName();
         return [
-            //
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->email(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'postal_code' => $this->faker->postcode(),
         ];
     }
 }
