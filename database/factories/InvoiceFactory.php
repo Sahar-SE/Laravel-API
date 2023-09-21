69e99989297507b4bb4e311c2aa3527f14a7bd46
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-21 23:23:24 +0430

    Add invoice factory

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Invoice;
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class InvoiceFactory extends Factory
@@ -13,8 +15,13 @@ class InvoiceFactory extends Factory
      */
     public function definition()
     {
+        $status = $this->faker->randomElement(['Build', 'paid', 'void']);
         return [
-            //
+            'customer_id' => Customer::factory(),
+            'amount' => $this->faker->numberBetween(1000, 100000),
+            'status' => $status,
+            'build_date' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'paid_date' => $status == 'paid' ? $this->faker->dateTimeBetween('-1 year', 'now') : null,
         ];
     }
 }
