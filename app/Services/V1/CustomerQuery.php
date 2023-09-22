f86ed877e0357827764edae1c327593ad0dbe399
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-22 22:39:58 +0430

    Add api filtering query

--- app/Services/V1/CustomerQuery.php
+++ app/Services/V1/CustomerQuery.php
@@ -1,4 +1,27 @@
 <?php
 namespace App\Services\V1;
 
+// To access the Request we import it from Illuminate\Http\Request
 use Illuminate\Http\Request;
+
+
+class CustomerQuery  {
+  // because user can write anything in input search so we have to allow specific columns to be searched
+  protected $allowedParams = [
+    'name' => ['eq'],
+    'type' => ['eq'],
+    'email' => ['eq'],
+    'address' => ['eq'],
+    'city' => ['eq'],
+    'state' => ['eq'],
+    // postalCode equal to, greater than, less than
+    'postalCode' => ['eq', 'gt', 'lt'],
+  ];
+  // Transform the fields to Db columns speciallly for postalCode because we have changed it to postalCode from postal_code
+
+  protected $columnMap = [
+    'postalCode' => 'postal_code'
+  ];
+
+  protected
+}
