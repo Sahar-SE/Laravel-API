ee44b490280be8c381ded10182576c2922c961b1
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-22 17:45:02 +0430

    Add api resource

--- app/Http/Resources/V1/CustomerResource.php
+++ app/Http/Resources/V1/CustomerResource.php
@@ -14,6 +14,8 @@ class CustomerResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+          'id' => $this->id
+        ];
     }
 }
