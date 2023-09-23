e23b887cb154f6ff23a81e24d243dfd0fbba74ea
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-23 18:00:19 +0430

    Add api filtering query

--- app/Filters/ApiFilter.php
+++ app/Filters/ApiFilter.php
@@ -1,3 +1,53 @@
 <?php
 
-namespace App\Filters\ApiFilter;
+namespace App\Filters;
+
+// To access the Request we import it from Illuminate\Http\Request
+use Illuminate\Http\Request;
+
+
+class CustomerFilter extends ApiFilter {
+  // because user can write anything in input search so we have to allow specific columns to be searched
+  protected $safeParams = [
+    'name' => ['eq'],
+    'type' => ['eq'],
+    'email' => ['eq'],
+    'address' => ['eq'],
+    'city' => ['eq'],
+    'state' => ['eq'],
+    // postalCode equal to, greater than, less than
+    'postalCode' => ['eq', 'gt', 'lt'],
+  ];
+  // Transform the fields to Db columns speciallly for postalCode because we have changed it to postalCode from postal_code
+
+  protected $columnMap = [
+    'postalCode' => 'postal_code'
+  ];
+
+  protected $opratorsMap = [
+    'eq' => '=',
+    'gt' => '>',
+    'lt' => '<'
+  ];
+
+  public function transform(Request $request) {
+    // eloquect query array
+    $eloQuery = [];
+    // param is the field name
+    foreach ($this->safeParams as $param => $operators) {
+      $query = $request->query($param);
+
+      if(!isset($query)) {
+        continue;
+      }
+
+      $column = $this->columnMap[$param] ?? $param;
+      foreach ($operators as $operator) {
+        $eloQuery[] = [$column, $this->opratorsMap[$operator], $query];
+      }
+    }
+
+    return $eloQuery;
+  }
+  
+}
