d35633b7493330f0626bfe1af6d2ecd5758709cc
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-23 17:50:14 +0430

    Add api filtering query

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,7 +8,7 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
-use App\Filters\V1\CustomerQuery;
+use App\Filters\V1\CustomerFilter;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
@@ -20,7 +20,7 @@ class CustomerController extends Controller
      */
     public function index(Request $request)
     {
-      $filter = new CustomerQuery();
+      $filter = new CustomerFilter();
       $queryItems = $filter->transform($request); //[['column', 'operator', 'value']]
 
       if($queryItems ==  0){
