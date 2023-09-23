f927a391d6f42d9e393770c2046102f1a6dac4de
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-23 17:47:32 +0430

    Add api filtering query

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,7 +8,7 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
-use App\Services\V1\CustomerQuery;
+use App\Filters\V1\CustomerQuery;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
