ef1d5eec100c7539d201211d4fe2693aeb466504
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-23 19:20:46 +0430

    Add api create query

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,6 +9,17 @@ class Customer extends Model
 {
   use HasFactory;
 
+  // The fields that user can fill it with data
+  protected $fillable = [
+    'name',
+    'type',
+    'email',
+    'address',
+    'city',
+    'state',
+    'postal_code'
+  ];
+
   public function invoices() {
     return $this->hasMany(Invoice::class);
   }
