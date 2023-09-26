cd48d593c5a95593dffac13fbbbf535831b495bb
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-26 20:45:42 +0430

    Update query

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -32,46 +32,23 @@ class CustomerController extends Controller
       
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreCustomerRequest  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreCustomerRequest $request)
     {
       return new CustomerResource(Customer::create($request->all()));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Customer $customer)
     {
       return new CustomerResource($customer);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateCustomerRequest  $request
-     * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-      
+      $customer->update($request->all());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Customer $customer)
     {
         //
