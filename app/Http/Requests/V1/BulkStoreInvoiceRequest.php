17c39dbfda6d45d66e9f81e9313bfc978e101e6c
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-27 20:10:02 +0430

    Update Bulk query

--- app/Http/Requests/V1/BulkStoreInvoiceRequest.php
+++ app/Http/Requests/V1/BulkStoreInvoiceRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests\V1;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class BULKStoreInvoiceRequest extends FormRequest
+class BulKStoreInvoiceRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
