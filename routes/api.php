cdd093b1e56ccfc1cd4da631dc37e6f2e74465f0
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-27 18:54:04 +0430

    Update Bulk query

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['prefix'=> 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
   Route::apiResource('customers', CustomerController::class);
   Route::apiResource('invoices', InvoiceController::class);
+  Route::post('invoices/bulk', ['uses' => 'InvoiceController@bulkStore']);
 });
