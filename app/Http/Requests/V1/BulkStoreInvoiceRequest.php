7fa2868f75d89e6ed070bc6add902b464bb5d379
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-09-27 19:04:11 +0430

    Update Bulk query

---
+++ app/Http/Requests/V1/BulkStoreInvoiceRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class BULKStoreInvoiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            // Validation for front-end form
+            'name' => ['required'],
+            'type' => ['required', Rule::in(['individual', 'business', 'I', 'B', 'i', 'b'])],
+            'email' => ['required', 'email'],
+            'address' => ['required'],
+            'city' => ['required'],
+            'state' => ['required'],
+            'postalCode' => ['required'],
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'postal_code' => $this->postalCode,
+        ]);
+    }
+}
